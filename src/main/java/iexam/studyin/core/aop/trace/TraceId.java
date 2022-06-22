114b62d79d999397718d812589593f5f8bf3956f
Lee-seungju <seungju0000@naver.com>
2022-06-22 20:14:26 +0900

    Aspect를 이용한 LogTrace 추가

---
+++ src/main/java/iexam/studyin/core/aop/trace/TraceId.java
@@ -0,0 +1,34 @@
+package iexam.studyin.core.aop.trace;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class TraceId {
+
+    private int level;
+    private String id;
+
+    public TraceId() {
+        this.level = 0;
+        this.id = UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public TraceId(int level, String id) {
+        this.level = level;
+        this.id = id;
+    }
+
+    public TraceId nextId() {
+        return new TraceId(level + 1, id);
+    }
+
+    public boolean isFirst() {
+        return level == 0;
+    }
+
+    public TraceId beforeId() {
+        return new TraceId(level - 1, id);
+    }
+}
