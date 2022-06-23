1ddd051ce44127afc9931005241a9ef765682561
Lee-seungju <seungju0000@naver.com>
2022-06-23 17:16:10 +0900

    테스트

--- src/main/java/iexam/studyin/core/config/SecurityConfig.java
+++ src/main/java/iexam/studyin/core/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeHttpRequests()
-                .antMatchers("/api/member/login/**","/api/home", "/api/member/email/**", "/api/member/signUpConfirm/**").permitAll()
+                .antMatchers("/api/member/login/**","/api/home", "/api/member/email/**", "/api/member/signUpConfirm/**", "/test/test").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
